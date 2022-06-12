9470c4f1487710c33e85ccddf9b2b0fa946d38ee
chendj89 <chendj89@hotmail.com>
2022-06-12 15:05:11 +0800

    2

--- docs/.vuepress/theme/layouts/components/vp-container.vue
+++ docs/.vuepress/theme/layouts/components/vp-container.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="vp-container" :data-name="path">
-    <vp-demo v-bind="props"></vp-demo>
+    <vp-demo v-if="show" v-bind="props"></vp-demo>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref, nextTick, onMounted } from "vue";
 const props = defineProps({
   source: String,
   path: String,
   description: String
 });
+const show = ref(false);
+onMounted(() => {
+  show.value = true;
+})
 </script>
 
 <style scoped>
